687ee20da0792357a8702b672c4a747c0210b9d0
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2024-09-17 15:35:13 -0400

    Add storage front and backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,36 @@
 "use client";
-
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
+import * as React from "react";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
+import { FileUploader } from "@aws-amplify/ui-react-storage";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
-
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
+  const onUploadStart = React.useCallback(() => {
+    console.log("uploading");
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
+  const onUploadSuccess = React.useCallback(() => {
+    console.log("uploaded");
+  }, []);
 
+  const onUploadError = React.useCallback(() => {
+    console.log("error");
+  }, []);
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
+      <FileUploader
+        isResumable
+        onUploadStart={onUploadStart}
+        onUploadSuccess={onUploadSuccess}
+        onUploadError={onUploadError}
+        maxFileCount={1}
+        path={`public/`}
+        acceptedFileTypes={["image/*"]}
+      />
     </main>
   );
 }
